36c363ad77f8ad28cf5fb6fdea9c83791a887f0d
Jody Brzovski <jbrzovski@cosmotecs.com>
2021-01-15 16:42:45 -0500

    finished functions and formatting for 5-day forcast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,9 @@
 let APIKey = "166a433c57516f51dfab1f7edaed8413";
-let uvIndex = "";  //hold the UV Index's
-
 
 // Here we are building the URL we need to query the One call weather API to retrieve the UV Index.  
 //This API requires Geographical coordinates that we get from the 5 day API so we can retirive the 
 //UV Index for the current day and the next 5 days and store them in the uvInxex array
-function getUVIndex(lat, lon) {
+function getWeatherData(lat, lon, city) {
 
     var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=,minutely,hourly,alerts&appid=" + APIKey;
 
@@ -17,29 +15,10 @@ function getUVIndex(lat, lon) {
         // We store all of the retrieved data inside of an object called "response"
         .then(function (response) {
 
-            //get the current uv incex and store in the uvIndex.current array 
-            uvIndex = response.current.uvi;
-            
-            var bgColor = "";  //holds the background color for UV Index
-            var textColor = "";  //holds the text color for UV Index
+            console.log(response);
 
-            if (uvIndex < 3) //if uv index is low (1-2)
-            {
-                bgColor = "bg-success";
-                textColor = "text-light";  
-            }
-            else if (uvIndex > 2 && uvIndex < 6)  //if uv index is mocerate (3-5)
-            {
-                bgColor = "bg-warning";
-                textColor = "text-dark";             
-            }
-            else  //if uv index is high (6+)
-            {
-                bgColor = "bg-danger";
-                textColor = "text-light";            
-            }
+            showWeatherData(response, city);
 
-            $("#currentUVIndex").html(uvIndex).addClass(bgColor + " p-1 " +  textColor); //set the UVIndex and color to the html
         });           
  };
 
@@ -59,16 +38,15 @@ function loadWeatherZip(zipCpde) {
         .then(function (response) {
 
             //load weather
-            showWeatherData(response);
+            getWeatherData(response.city.coord.lat, response.city.coord.lon, response.city.name);
 
         });
 }
 
-function loadWeatherCity(strZip) {
+function loadWeatherCity(city) {
 
     
-    zipCode = strZip;
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=" + zipCode + ",us&appid=" + APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&appid=" + APIKey;
     var weatherContainer = $("#weatherContainer");
 
     // Here we run our AJAX call to the OpenWeatherMap API
@@ -79,31 +57,80 @@ function loadWeatherCity(strZip) {
         // We store all of the retrieved data inside of an object called "response"
         .then(function (response) {
             //load weather
-            showWeatherData(response);
+            getWeatherData(response.city.coord.lat, response.city.coord.lon, response.city.name);
         });
 }
 
-
-
-function showWeatherData(weatherData)
+function showWeatherData(weatherData, city)
 {
-    console.log(weatherData);
-    var iconURL = "http://openweathermap.org/img/w/" + weatherData.list[0].weather[0].icon + ".png";  //get weather icon
-    $("#cityDate").html(weatherData.city.name + " (" + new Date().toLocaleDateString() + ") <img id=\"icon\" src=\"" + iconURL  + "\" alt=\"Weather icon\"/>");
+    //load current
+    var iconURL = "http://openweathermap.org/img/w/" + weatherData.current.weather[0].icon + ".png";  //get weather icon
+    $("#cityDate").html(city + " (" + new Date().toLocaleDateString() + ") <img id=\"icon\" src=\"" + iconURL  + "\" alt=\"Weather icon\"/>");
 
-    var temp = parseInt(weatherData.list[0].main.temp);
+    var temp = parseInt(weatherData.current.temp);
     temp = Math.round(((temp-273.15)*1.8) + 32);
     $("#currentTemp").html(" " + temp +  "  &degF");
-    $("#currentHumidity").html(weatherData.list[0].main.humidity + "%");
-    $("#currentWindSpeed").html(weatherData.list[0].wind.speed + " MPH");
-    getUVIndex(weatherData.city.coord.lat, weatherData.city.coord.lon);
+    $("#currentHumidity").html(weatherData.current.humidity + "%");
+    $("#currentWindSpeed").html(weatherData.current.wind_speed + " MPH");
 
-    $("#currentWeather").show();
+    //get the current uv index and store in the uvIndex.current array 
+    var uvIndex = weatherData.current.uvi;
+
+    var bgColor = "";  //holds the background color for UV Index
+    var textColor = "";  //holds the text color for UV Index
+
+    if (uvIndex < 3) //if uv index is low (1-2)
+    {
+        bgColor = "bg-success";
+        textColor = "text-light";  
+    }
+    else if (uvIndex > 2 && uvIndex < 6)  //if uv index is mocerate (3-5)
+    {
+        bgColor = "bg-warning";
+        textColor = "text-dark";             
+    }
+    else  //if uv index is high (6+)
+    {
+        bgColor = "bg-danger";
+        textColor = "text-light";            
+    }
+
+    $("#currentUVIndex").html(uvIndex).addClass(bgColor + " p-1 " +  textColor); //set the UVIndex and color to the html
+
+
+    //load 5 Day
+    var ul5 = $("#fiveDay");
+    ul5.empty();
+
+    for (i=1; i < 6; i++)
+    {
+        var div = $("<div>").addClass("bg-primary");
+
+        var dateTime = parseInt(weatherData.daily[i].dt);
+        var dateHeading = $("<h6>").text(new Date(dateTime * 1000).toLocaleDateString());
+        var iconDayURL = "http://openweathermap.org/img/w/" + weatherData.daily[i].weather[0].icon + ".png";  //get weather icon
+        var icon = $("<img>").attr("src", iconDayURL);
+
+        temp = parseInt(weatherData.daily[i].temp.day);
+        temp = Math.round(((temp-273.15)*1.8) + 32);
+        var temp5 = $("<p>").html("Temp: " + temp +  "  &degF");
+
+        var humidity5 = $("<p>").html("Humidity: " + weatherData.daily[i].humidity + "%");
+
+        div.append(dateHeading);
+        div.append(icon);
+        div.append(temp5);
+        div.append(humidity5);
+        ul5.append(div);
+
+    }
+
+    $("#weatherData").show();
 }
 
 $(document).ready(function () {
 
-    $("#currentWeather").hide();
+    $("#weatherData").hide();
 
     $("#searchBtn").click(function (event) {
         var element = event.target; //set element to the div that was clicked
